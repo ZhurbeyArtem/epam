5d72c1399c68b1fe51988e2effda9c8208ca377c
Zhurbey Artem <zhurbeyartem@gmail.com>
2024-02-24 19:16:44 +0200

    fix

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -3,17 +3,20 @@ import { createBrowserRouter } from "react-router-dom";
 import { Home } from "../pages/Home/Home";
 import { Tweets } from "../pages/Tweets/Tweets";
 
-export const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/tweets",
-    element: <Tweets />,
-  },
-  {
-    path: "*",
-    element: <Home />,
-  },
-]);
+export const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/tweets",
+      element: <Tweets />,
+    },
+    {
+      path: "*",
+      element: <Home />,
+    },
+  ]
+
+);
